bc625d246436d04eeb9a096f6a0671ea7db4a24a
yunpeng.zhan <yunpeng.zhan@flexiv.com>
2023-03-09 16:25:50 +0800

    update

--- src/poetry/utils/env.py
+++ src/poetry/utils/env.py
@@ -1209,9 +1209,9 @@ class EnvManager:
         sanitized_name = re.sub(r'[ $`!*@"\\\r\n\t]', "_", name)[:42]
         normalized_cwd = os.path.normcase(os.path.realpath(cwd))
         h_bytes = hashlib.sha256(encode(normalized_cwd)).digest()
-        h_str = base64.urlsafe_b64encode(h_bytes).decode()[:8]
+        # h_str = base64.urlsafe_b64encode(h_bytes).decode()[:8]
 
-        return f"{sanitized_name}-{h_str}"
+        return f"{sanitized_name}"
 
 
 class Env:
